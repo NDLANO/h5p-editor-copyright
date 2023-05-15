337f173d562da252c429ecdf74c24b0e078638f6
Yijun Stautland <github@yps.staut.land>
2023-05-15 11:49:35 +0200

    Update webpack config to fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,13 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            allowTsInNodeModules: true,
+          },
+        },
+        exclude: /node_modules\/(?!(h5p-utils)\/).*/,
       },
     ],
   },
